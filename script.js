55b8b915a995ed54120ffbfe9680ecd1520ff3f6
AJ <firesmasher231@users.noreply.github.com>
2024-05-21 19:48:19 +0100

    prettier

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
 function generateCSV() {
-    var text = document.getElementById('inputText').value;
-    var lines = text.split('\n');
-    var csvContent = "data:text/csv;charset=utf-8," + '\uFEFF'; // UTF-8 BOM for Excel compatibility
+	var text = document.getElementById("inputText").value;
+	var lines = text.split("\n");
+	var csvContent = "data:text/csv;charset=utf-8," + "\uFEFF"; // UTF-8 BOM for Excel compatibility
 
-    lines.forEach(function(line) {
-        csvContent += line + '\r\n';
-    });
+	lines.forEach(function (line) {
+		csvContent += line + "\r\n";
+	});
 
-    var encodedUri = encodeURI(csvContent);
-    var link = document.getElementById('downloadLink');
-    link.setAttribute("href", encodedUri);
-    link.setAttribute("download", "translations.csv");
-    link.textContent = "Download CSV";
-    link.style.display = 'block';
+	var encodedUri = encodeURI(csvContent);
+	var link = document.getElementById("downloadLink");
+	link.setAttribute("href", encodedUri);
+	link.setAttribute(
+		"download",
+		"translations " + new Date().toISOString().slice(0, 10) + ".csv"
+	);
+	link.textContent = "Download CSV";
+	link.click();
 }
